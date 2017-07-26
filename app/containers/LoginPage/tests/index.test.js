94ec30b3196137c33ae7e84ac1339cbe1a08f842
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2017-07-26 11:52:16 -0700

    include a loginPage test

--- app/containers/LoginPage/tests/index.test.js
+++ app/containers/LoginPage/tests/index.test.js
@@ -1,10 +1,22 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { LoginPage } from '../index';
+import { LoginPage } from '../index';
+
+// describe('<LoginPage />', () => {
+//   it('Expect to have unit tests specified', () => {
+//     expect(true).toEqual(false);
+//   });
+// });
 
 describe('<LoginPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should render its children', () => {
+    const children = (<h1>Test</h1>);
+    const renderedComponent = shallow(
+      <LoginPage locale="en">
+        {children}
+      </LoginPage>
+    );
+    expect(renderedComponent.contains(children)).toBe(true);
   });
 });
